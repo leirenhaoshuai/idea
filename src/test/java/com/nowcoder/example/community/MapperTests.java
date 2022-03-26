c9a67e126d7466675b0f93708770cd3052d5cf12
leiyinzhao <1798592941@qq.com>
2022-03-26 13:57:55 +0800

    ffff

---
+++ src/test/java/com/nowcoder/example/community/MapperTests.java
@@ -0,0 +1,53 @@
+package com.nowcoder.example.community;
+
+import com.nowcoder.example.community.dao.DiscussPostMapper;
+import com.nowcoder.example.community.dao.UserMapper;
+import com.nowcoder.example.community.entity.DiscussPost;
+import com.nowcoder.example.community.entity.User;
+import com.nowcoder.example.community.CommunityApplication;
+import com.nowcoder.example.community.dao.UserMapper;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.Date;
+import java.util.List;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@ContextConfiguration(classes = CommunityApplication.class)
+public class MapperTests {
+
+    @Autowired
+    private UserMapper userMapper;
+
+    @Autowired
+    private DiscussPostMapper discussPostMapper;
+
+    @Test
+    public void testSelectUser() {
+        int rows = userMapper.updateStatus(150, 1);
+        System.out.println(rows);
+
+        rows = userMapper.updateHeader(150, "http://www.nowcoder.com/102.png");
+        System.out.println(rows);
+
+        rows = userMapper.updatePassword(150, "hello");
+        System.out.println(rows);
+    }
+
+    @Test
+    public void testSelectPosts() {
+        List<DiscussPost> list = discussPostMapper.selectDiscussPosts(149, 0, 10);
+        for(DiscussPost post : list) {
+            System.out.println(post);
+        }
+
+        int rows = discussPostMapper.selectDiscussPostRows(149);
+        System.out.println(rows);
+    }
+
+}
